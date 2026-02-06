ff0e6b62ce89edb3cb61424aa0babb61530874a2
srj9 <serejhaaa@gmail.com>
2026-02-06 17:43:59 +0400

    Переход на переменные среды - убрал значения по умолчанию

--- plant_monitoring/settings.py
+++ plant_monitoring/settings.py
@@ -60,11 +60,11 @@ TEMPLATES = [
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME', 'plant_monitoring'),
-        'USER': os.getenv('DB_USER', 'sensor_user'),
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST', 'localhost'),
-        'PORT': os.getenv('DB_PORT', '5432'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
